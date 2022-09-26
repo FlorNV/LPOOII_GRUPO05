03182fa1da42013b1c90eb78b2dc4bf63eb5b68a
FlorNV <florencianoelivaldez@gmail.com>
2022-09-26 19:54:56 -0300

    probando punto3

---
+++ Vistas/ConversionStringToBrush.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+using System.Drawing;
+
+namespace Vistas
+{
+    class ConversionStringToBrush : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Brush oBrush = null;
+            switch (value.ToString())
+            {
+                case "PENDIENTE":
+                    oBrush = Brushes.Red;
+                    break;
+                case "PAGADA":
+                    oBrush = Brushes.Green;
+                    break;
+                case "CONTABILIZADA":
+                    oBrush = Brushes.Blue;
+                    break;
+                case "ANULADA":
+                    oBrush = Brushes.Gray;
+                    break;
+            }
+            return oBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
