2f595c233e48ff96ff2cf5cca7df7a940b3bb11a
briansleonel <gonzalezbrianleonel76@gmail.com>
2022-09-28 12:09:40 -0300

    Se Imeplementa la interfad IDataErrorInfo

--- ClasesBase/Producto.cs
+++ ClasesBase/Producto.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+
 namespace ClasesBase {
-    public class Producto {
+    public class Producto : IDataErrorInfo {
         private string codProducto;
 
         public string CodProducto {
@@ -34,6 +37,47 @@ namespace ClasesBase {
         public decimal Precio {
             get { return precio; }
             set { precio = value; }
+        }
+
+        public string Error {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName] {
+            get {
+                string result = null;
+                if (columnName == "CodProducto") { 
+                    if(!String.IsNullOrEmpty(CodProducto)) {
+                        result = "Debe ingresar el Código del Producto";
+                    } else if (CodProducto.Length < 3) {
+                        result = "Debe tener al menos 3 letras";
+                    }
+                } else if (columnName == "Categoria") {
+                    if (String.IsNullOrEmpty(Categoria)) {
+                        result = "Debe ingresar la Categoría del Producto";
+                    }
+                } else if (columnName == "Color") {
+                    if (String.IsNullOrEmpty(Color)) {
+                        result = "Debe ingresar el Color del Producto";
+                    }
+                } else if (columnName == "Descripcion") {
+                    if (String.IsNullOrEmpty(Descripcion)) {
+                        result = "Debe ingresar la Descripción del Producto";
+                    }
+                } 
+                /*else if (columnName == "Precio") {
+                    decimal num;
+                    if (Precio == null) {
+                        result = "Debe ingresar el Precio del Producto";
+                    } else if (!decimal.TryParse(Precio.ToString(), out num)) {
+                        result = "Debe ingresar un número";
+                    } else if (Precio < 0) {
+                        result = "El Precio debe ser mayor a $0.00";
+                    }
+                } */
+
+                return result;
+            }
         }
     }
 }
