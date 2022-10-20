90894460f43b9702ba19723aee3c409fc3bb1c47
sofia-munoz <sofim.2803.1@gmail.com>
2022-10-20 20:04:25 -0200

    Agrego la plantilla del comprobante

---
+++ Vistas/Comprobante.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for Comprobante.xaml
+    /// </summary>
+    public partial class Comprobante : Window
+    {
+        public Comprobante()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
